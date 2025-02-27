ee96fc1e331691e70dac788019815f1dd5c342be
m1cha1s <msulej04@gmail.com>
2025-02-28 00:44:55 +0100

    Continued development
    Started to implement the actual layout function as well as understanding
    more of what Ryan Fleury meant in his blog posts.

--- main.c
+++ main.c
@@ -4,32 +4,39 @@
 
 #include <raylib.h>
 
+#define MS_ARENA_IMPLEMENTATION
+#include "ms_arena.h"
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+#undef STB_DS_IMPLEMENTATION
 #define IMPL
-#include "base.h"
 #include "ui.h"
 
+Arena temp_arena = {0};
 
 int main() {
-    SetWindowState(FLAG_WINDOW_RESIZABLE 
-                   | FLAG_MSAA_4X_HINT 
-                   | FLAG_VSYNC_HINT 
-                   | FLAG_WINDOW_HIGHDPI);
+    SetWindowState(FLAG_WINDOW_RESIZABLE
+                   | FLAG_MSAA_4X_HINT
+                   | FLAG_VSYNC_HINT
+                   | FLAG_WINDOW_HIGHDPI
+                   | FLAG_WINDOW_ALWAYS_RUN);
     InitWindow(800, 600, "UI Fun");
 
     UI_State state;
 
-    UI_InitState(&state);
-    UI_SetState(&state);
+    ui_init_state(&state);
+    ui_set_state(&state);
 
     while (!WindowShouldClose()) {
-        DeferLoop(BeginDrawing(), EndDrawing()) {
-            ArenaScope(temp_arena) {
-                UI_BeginFrame(); // State agnostic...
+        BeginDrawing();
 
-                ClearBackground(WHITE);
-                DrawText("Hello world!", 20, 20, 20, BLACK);
-            }
-        }
+            ui_begin_frame(); // State agnostic...
+
+            ClearBackground(WHITE);
+            DrawText("Hello world!", 20, 20, 20, BLACK);
+
+            arena_reset(&temp_arena);
+        EndDrawing();
     }
 
     CloseWindow();
--- ui.h
+++ ui.h
@@ -1,204 +1,99 @@
 #ifndef _UI_H
 #define _UI_H
 
-#include <stdarg.h>
+#include <stddef.h>
+//#include <stdint.h>
+#include <string.h>
 
-#include "base.h"
+typedef struct String {
+    char *str;
+    size_t len;
+} String;
 
-typedef struct UI_Key {
-    String8 literal;
-    u32 hash;
-} UI_Key;
+typedef enum UI_Axis2 {
+    UI_Axis2_x,
+    UI_Axis2_y,
+    UI_Axis2_COUNT,
+} UI_Axis2;
 
-typedef struct Rng2f32 {
-    f32 x1, y1, x2, y2;
-} Rng2f32;
+typedef struct Rect {
+    float xy[2], wh[2];
+} Rect;
 
-typedef enum UI_SizeKind {
-    UI_SizeKind_NULL,
-    UI_SizeKind_Pixels,
-    UI_SizeKind_TextContent,
-    UI_SizeKind_PercentOfParent,
-    UI_SizeKind_ChildrenSum,
-} UI_SizeKind;
+typedef enum UI_Sem_Size {
+    UI_Sem_Size_Fill,
+    UI_Sem_Size_Parent_Percent,
+    UI_Sem_Size_Text_Fit;
+} UI_Sem_Size;
 
-typedef struct UI_Size {
-    UI_SizeKind kind;
-    f32 value;
-    f32 strictness;
-} UI_Size;
-
-typedef enum Axis2 {
-    Axis2_X,
-    Axis2_Y,
-    Axis2_COUNT,
-} Axis2;
-
-typedef u32 UI_NodeFlags;
+typedef int UI_Flags;
 enum {
-    UI_NodeFlag_Clickable = (1<<0),
-    UI_NodeFlag_ViewScroll = (1<<1),
-    UI_NodeFlag_DrawText = (1<<2),
-    UI_NodeFlag_DrawBorder = (1<<3),
-    UI_NodeFlag_DrawBackground = (1<<4),
-    UI_NodeFlag_DrawDropShadow = (1<<5),
-    UI_NodeFlag_Clip = (1<<6),
-    UI_NodeFlag_HotAnimation = (1<<7),
-    UI_NodeFlag_ActiveAnimation = (1<<8),
+    UI_CENTER_X     = (1<<0),
+    UI_CENTER_Y     = (1<<1),
+    UI_ALIGN_LEFT   = (1<<2),
+    UI_ALIGN_RIGHT  = (1<<3),
+    UI_ALIGN_TOP    = (1<<4),
+    UI_ALIGN_BOTTOM = (1<<5),
+
+    UI_DRAW_TEXT       = (1<<6),
+    UI_DRAW_BACKGROUND = (1<<7),
+    UI_DRAW_FRAME      = (1<<8),
 };
 
 typedef struct UI_Node UI_Node;
 struct UI_Node {
-    UI_Node *first;
-    UI_Node *last;
+    UI_Node *first_child;
     UI_Node *next;
-    UI_Node *prev;
-    UI_Node *parent;
 
-    // Per-frame info provided by builders
-    UI_NodeFlags flags;
-    String8 string;
-    UI_Size semantic_size[Axis2_COUNT];
+    size_t child_count;
 
-    // Computed every frame
-    f32 computed_rel_position[Axis2_COUNT];
-    f32 computed_size[Axis2_COUNT];
-    Rng2f32 rect;
+    UI_Sem_Size sem_size[UI_Axis2_COUNT];
+    // float desired_size[UI_Axis2_COUNT];
 
-    UI_Node *hash_next;
-    UI_Node *hash_prev;
+    UI_Flags flags;
 
-    UI_Key key;
-    u64 last_frame_touched_index;
+    char *string;
 
-    // persistant
-    f32 hot_t;
-    f32 active_t;
+    Rect dim;
 };
 
-typedef struct UI_Comm {
-    UI_Node *node;
-    Vec2f32 mouse;
-    Vec2f32 drag_delta;
-    b8 clicked;
-    b8 double_clicked;
-    b8 right_clicked;
-    b8 pressed;
-    b8 released;
-    b8 dragging;
-    b8 hovering;
-} UI_Comm;
+size_t hash_string(char *str, size_t len);
+#define hash_String(str) hash_string((str).str, (str).len)
 
-typedef struct UI_State {
-    Arena *arena;
-
-    UI_Node *hash_first, *hash_last;
-} UI_State;
-extern UI_State *ui_state;
-
-void UI_InitState(UI_State *state);
-void UI_SetState(UI_State *state);
-
-void UI_BeginFrame(void);
-
-// Helpers
-UI_Key UI_KeyNull(void);
-UI_Key UI_KeyFromString(String8 string);
-b32 UI_KeyMatch(UI_Key a, UI_Key b);
-
-UI_Node *UI_NodeMake(UI_NodeFlags flags, String8 string);
-UI_Node *UI_NodeMakeF(UI_NodeFlags flags, char *fmt, ...);
-
-void UI_NodeEquipDisplayString(UI_Node *node, String8 string);
-// void UI_NodeEquipChildLayoutAxis(UI_Node *node, Axis2 axis);
-
-UI_Node *UI_PushParent(UI_Node *node);
-UI_Node *UI_PopParent(void);
-
-UI_Comm UI_CommFromNode(UI_Node *node);
+void ui_layout(UI_Node *node);
 
 #ifdef IMPL
 
-UI_State *ui_state;
-u64 ui_frame_index;
-
-void UI_InitState(UI_State *state) {
-    MemZeroPtr(state);
-}
-
-void UI_SetState(UI_State *state) {
-    ui_state = state;
-}
-
-void UI_BeginFrame(void) {
-    ui_frame_index += 1;
-}
-
-// Helpers
-UI_Key UI_KeyNull(void) {
-    return (UI_Key){0};
-}
-
-UI_Key UI_KeyFromString(String8 string) {
-    return (UI_Key){
-        .literal = string,
-        .hash = Hash_String8(string),
-    };
-}
-
-b32 UI_KeyMatch(UI_Key a, UI_Key b) {
-    return a.hash == b.hash; // This may not be correct...
-}
-
-
-
-
-UI_Node *UI_NodeMake(UI_NodeFlags flags, String8 string) {
-    UI_Node *node = ArenaAlloc(ui_state->arena, sizeof(UI_Node));
-
-    MemZeroPtr(node);
-
-    node->flags = flags;
-    node->key = UI_KeyFromString(string);
-
-    if (!ui_state->hash_first || !ui_state->hash_last) {
-        ui_state->hash_first = ui_state->hash_last = node;
-    } else {
-        ui_state->hash_last->hash_next = node;
-        node->prev = ui_state->hash_last;
-        ui_state->hash_last = node;
+size_t hash_string(char *str, size_t len) {
+    size_t hash = 2166136261u;
+    for (size_t i = 0; i < len; ++i) {
+        hash ^= (unsigned char)str[i];
+        hash *= 16777619;
     }
-
-    return node;
+    return hash;
 }
 
-// TODO: Implement the formatting
-UI_Node *UI_NodeMakeF(UI_NodeFlags flags, char *fmt, ...) {
-    String8 str = String8_Slice(fmt, CStrLen(fmt));
-    return UI_NodeMake(flags, str);
-}
+void ui_layout(UI_Node *root)
+{
+    UI_Node *child = root->first_child;
+    for (size_t i = 0; i < root->child_count; ++i) {
+        for (int ax = UI_Axis2_x; ax < UI_Axis2_COUNT; ++ax)
+        {
+            // 1st: Sizing
+            switch (child->sem_size[ax])
+            {
+            case UI_Sem_Size_Fill:
+                child->dim.wh[ax] = root->dim.wh[ax]/root->child_count;
+                break;
+            // TODO: Add more...
+            default:
+                break;
+            }
+            // 2nd: Positioning
 
-
-void UI_NodeEquipDisplayString(UI_Node *node, String8 string) {
-    node->string = string;
-}
-
-// void UI_NodeEquipChildLayoutAxis(UI_Node *node, Axis2 axis) {
-//     node->
-// }
-
-
-UI_Node *UI_PushParent(UI_Node *node) {
-
-}
-
-UI_Node *UI_PopParent(void) {
-
-}
-
-
-UI_Comm UI_CommFromNode(UI_Node *node) {
-
+        }
+        child = child->next;
+    }
 }
 
 #endif
